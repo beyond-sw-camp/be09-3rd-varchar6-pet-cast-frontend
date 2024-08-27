a001cc5943e8c8264e79ce95e0bbb3e4570f9809
gilhan <rlfgks97@gmail.com>
2024-08-27 14:12:53 +0900

    fix: update mapper class to static method

--- backend/src/main/java/com/varchar6/petcast/domain/member/command/application/service/MemberService.java
+++ backend/src/main/java/com/varchar6/petcast/domain/member/command/application/service/MemberService.java
@@ -1,7 +1,7 @@
 package com.varchar6.petcast.domain.member.command.application.service;
 
+import com.varchar6.petcast.domain.member.command.domain.aggregate.Member;
 import com.varchar6.petcast.domain.member.command.domain.repository.MemberRepository;
-import com.varchar6.petcast.domain.member.command.application.dto.mapper.MemberMapperTemplate;
 import com.varchar6.petcast.domain.member.command.application.dto.request.MemberRequestDTO;
 import com.varchar6.petcast.domain.member.command.application.dto.response.MemberResponseDTO;
 import jakarta.transaction.Transactional;
@@ -9,24 +9,65 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
 @Slf4j
 @Service(value="commandMemberService")
 public class MemberService {
     private final MemberRepository memberRepository;
-    private final MemberMapperTemplate memberMapperTemplate;
+
+    private static final String FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(FORMAT);
 
     @Autowired
-    public MemberService(MemberRepository memberRepository, MemberMapperTemplate memberMapperTemplate) {
+    public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
-        this.memberMapperTemplate = memberMapperTemplate;
     }
 
     @Transactional
     public MemberResponseDTO registerMember(MemberRequestDTO memberRequestDTO) {
-        return memberMapperTemplate.entityToResponseDTO(
+        return entityToResponseDTO(
                 memberRepository.save(
-                        memberMapperTemplate.requestDTOToEntity(memberRequestDTO)
+                        requestDTOToEntity(memberRequestDTO)
                 )
         );
+    }
+
+    public static Member requestDTOToEntity(MemberRequestDTO memberRequestDTO) {
+        return Member.builder()
+                .loginId(memberRequestDTO.getLoginId())
+                .password(memberRequestDTO.getPassword())
+                .name(memberRequestDTO.getName())
+                .phone(memberRequestDTO.getPhone())
+                .nickname(memberRequestDTO.getNickname())
+                .image(memberRequestDTO.getImage())
+                .createdAt(
+                        LocalDateTime.now()
+                                .format(FORMATTER)
+                )
+                .updatedAt(
+                        LocalDateTime.now()
+                                .format(FORMATTER)
+                )
+                .activeYn(true)
+                .introduction(memberRequestDTO.getIntroduction())
+                .build();
+    }
+
+    public static MemberResponseDTO entityToResponseDTO(Member member) {
+        return MemberResponseDTO.builder()
+                .loginId(member.getLoginId())
+                .password(member.getPassword())
+                .name(member.getName())
+                .phone(member.getNickname())
+                .nickname(member.getNickname())
+                .image(member.getImage())
+                .createdAt(member.getCreatedAt())
+                .updatedAt(member.getUpdatedAt())
+                .activeYn(member.isActiveYn())
+                .introduction(member.getIntroduction())
+                .build();
     }
 }
