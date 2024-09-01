4cd2dd03d65f5c2cf10df46af5ef95da85318b82
gilhan <rlfgks97@gmail.com>
2024-09-01 16:41:53 +0900

    fix: add PSA to MemberService

--- backend/src/main/java/com/varchar6/petcast/domain/member/query/service/MemberService.java
+++ backend/src/main/java/com/varchar6/petcast/domain/member/query/service/MemberService.java
@@ -1,6 +1,8 @@
 package com.varchar6.petcast.domain.member.query.service;
 
+import com.varchar6.petcast.domain.member.query.dto.MemberDTO;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface MemberService extends UserDetailsService {
+    MemberDTO getMemberInformationById(int memberId);
 }
