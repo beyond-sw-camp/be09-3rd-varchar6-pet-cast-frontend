7bda30261fd30255feb60ec00934d7068afd2c1e
JeongJunSeo99 <wnstj0810@naver.com>
2024-09-01 11:39:59 +0900

    feat: review Read

--- backend/src/main/java/com/varchar6/petcast/domain/review/query/mapper/ReviewMapper.java
+++ backend/src/main/java/com/varchar6/petcast/domain/review/query/mapper/ReviewMapper.java
@@ -1,8 +1,14 @@
 package com.varchar6.petcast.domain.review.query.mapper;
 
+import com.varchar6.petcast.domain.review.query.dto.ReviewDTO;
+import java.util.List;
+import java.util.Map;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public class ReviewMapper {
+public interface ReviewMapper {
 
+    List<ReviewDTO> selectReviewByCompanyId(Map<String, String> params);
+
+    List<ReviewDTO> selectReviewByCustomerId(Map<String, String> params);
 }
---
+++ backend/src/main/java/com/varchar6/petcast/domain/review/query/service/ReviewServiceImpl.java
@@ -0,0 +1,48 @@
+package com.varchar6.petcast.domain.review.query.service;
+
+import com.varchar6.petcast.common.response.ResponseMessage;
+import com.varchar6.petcast.domain.review.query.dto.ReviewDTO;
+import com.varchar6.petcast.domain.review.query.mapper.ReviewMapper;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Slf4j
+@Service(value = "queryReviewService")
+public class ReviewServiceImpl implements ReviewService {
+
+    private final ReviewMapper reviewMapper;
+
+    @Autowired
+    public ReviewServiceImpl(ReviewMapper reviewMapper) {
+        this.reviewMapper = reviewMapper;
+    }
+
+    @Override
+    public List<ReviewDTO> findReviewByCompany(Integer companyId, Integer lastReviewId) {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("companyId", String.valueOf(companyId));
+        params.put("lastReviewId", String.valueOf(lastReviewId));
+
+        List<ReviewDTO> reviewDTOS = reviewMapper.selectReviewByCompanyId(params);
+        return reviewDTOS;
+    }
+
+    @Override
+    public List<ReviewDTO> findEventByCustomer(Integer userId, Integer lastReviewId) {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("customerId", String.valueOf(userId));
+        params.put("lastReviewId", String.valueOf(lastReviewId));
+
+        List<ReviewDTO> reviewDTOS = reviewMapper.selectReviewByCustomerId(params);
+
+        return reviewDTOS;
+    }
+}
