6e203fb02c91cb5eb4e197a2e707afd7559bf10b
gilhan <rlfgks97@gmail.com>
2024-09-01 15:19:32 +0900

    feat: delete unused method handler & annotations for user
    [변경사항]
    
    가독성 향상을 위해 사용되지 않는 주석과 메소드 핸들러를 제거합니다.

--- backend/src/main/java/com/varchar6/petcast/domain/member/command/application/controller/MemberController.java
+++ backend/src/main/java/com/varchar6/petcast/domain/member/command/application/controller/MemberController.java
@@ -14,29 +14,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController(value = "commandMemberController")
-@RequestMapping("/api/v1/member")
+@RequestMapping("/api/v1/members")
 @Slf4j
 public class MemberController {
 
-    /* 설명. 의존성 주입 */
     private final MemberService memberService;
     private ModelMapper modelMapper;
-    private Environment env;
 
-    /* 설명. 매개변수 생성자 */
     @Autowired
-    public MemberController(MemberService memberService, ModelMapper modelMapper, Environment env) {
+    public MemberController(MemberService memberService, ModelMapper modelMapper) {
         this.memberService = memberService;
         this.modelMapper = modelMapper;
-        this.env = env;
     }
 
-    @GetMapping("/health")
-    public String status(){
-        return "I'm Working in User Service " + env.getProperty("local.server.port");
-    }
-
-    /* 설명. 회원 가입 */
     @PostMapping("/regist")
     public ResponseEntity<ResponseRegistUserVO> registMember(@RequestBody RequestRegistUserVO newUser){
 
