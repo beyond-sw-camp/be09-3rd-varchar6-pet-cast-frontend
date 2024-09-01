cfaec75163b70d0713dad80714e9edc3fc2ef821
gilhan <rlfgks97@gmail.com>
2024-09-01 22:19:59 +0900

    feat: add assign authority when user sign-up

--- backend/src/main/java/com/varchar6/petcast/domain/member/command/application/service/MemberServiceImpl.java
+++ backend/src/main/java/com/varchar6/petcast/domain/member/command/application/service/MemberServiceImpl.java
@@ -1,9 +1,12 @@
 package com.varchar6.petcast.domain.member.command.application.service;
 
 import com.varchar6.petcast.domain.member.command.domain.aggregate.Member;
+import com.varchar6.petcast.domain.member.command.domain.aggregate.RoleMember;
+import com.varchar6.petcast.domain.member.command.domain.aggregate.RoleType;
 import com.varchar6.petcast.domain.member.command.domain.repository.MemberRepository;
 import com.varchar6.petcast.domain.member.command.application.dto.request.MemberRequestDTO;
 import com.varchar6.petcast.domain.member.command.application.dto.response.MemberResponseDTO;
+import com.varchar6.petcast.domain.member.command.domain.repository.RoleRepository;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +24,7 @@ import java.time.format.DateTimeFormatter;
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
+    private final RoleRepository roleRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     private static final String FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
@@ -28,18 +32,30 @@ public class MemberServiceImpl implements MemberService {
 
     @Autowired
     public MemberServiceImpl(MemberRepository memberRepository,
-                             BCryptPasswordEncoder bCryptPasswordEncoder) {
+                             BCryptPasswordEncoder bCryptPasswordEncoder,
+                             RoleRepository roleRepository) {
         this.memberRepository = memberRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.roleRepository = roleRepository;
     }
 
     @Override
     @Transactional
     public MemberResponseDTO registerMember(MemberRequestDTO memberRequestDTO) {
-        log.info("memberRequestDTO.image: {}", memberRequestDTO.getImage());
+
+        // 비밀번호 암호화
         memberRequestDTO.setPassword(bCryptPasswordEncoder.encode(memberRequestDTO.getPassword()));
 
-        return entityToResponseDTO(memberRepository.save(requestDTOToEntity(memberRequestDTO)));
+        // Member 생성
+        Member member = memberRepository.save(requestDTOToEntity(memberRequestDTO));
+
+        // 회원 권한 부여
+        roleRepository.save(RoleMember.builder()
+                .memberId(member.getId())
+                .roleId(RoleType.CUSTOMER.getRoleId())
+                .build());
+
+        return entityToResponseDTO(member);
     }
 
     public static Member requestDTOToEntity(MemberRequestDTO memberRequestDTO) {
