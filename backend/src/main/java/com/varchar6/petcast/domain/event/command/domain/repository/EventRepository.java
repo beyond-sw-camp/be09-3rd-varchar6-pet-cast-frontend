9e8705b60871f1f9ff527abd72a7a21d530b9a0c
JeongJunSeo99 <wnstj0810@naver.com>
2024-09-02 17:46:45 +0900

    feat: event test code 작성 중

--- backend/src/main/java/com/varchar6/petcast/domain/event/command/domain/repository/EventRepository.java
+++ backend/src/main/java/com/varchar6/petcast/domain/event/command/domain/repository/EventRepository.java
@@ -5,10 +5,12 @@ import com.varchar6.petcast.domain.event.command.domain.aggregate.Event;
 import com.varchar6.petcast.domain.notice.command.domain.aggregate.Notice;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Integer> {
-
+    @Query("SELECT e FROM Event e ORDER BY e.id DESC LIMIT 1")
+    Event findLatestEvent();
 }
