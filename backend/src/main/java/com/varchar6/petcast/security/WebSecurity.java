7996fd4b4f36a2bf64c2c9e619203b2c16575e02
gilhan <rlfgks97@gmail.com>
2024-09-02 11:44:33 +0900

    chore

--- backend/src/main/java/com/varchar6/petcast/security/WebSecurity.java
+++ backend/src/main/java/com/varchar6/petcast/security/WebSecurity.java
@@ -57,9 +57,9 @@ public class WebSecurity {
 
         http.authorizeHttpRequests(authz ->
                 authz
-                        .requestMatchers(new AntPathRequestMatcher("/api/v1/**")).hasRole(Role.CUSTOMER.getType())     // for dev
-                        .requestMatchers(new AntPathRequestMatcher("/login")).permitAll()
-                        .requestMatchers(new AntPathRequestMatcher("/api/v1/members/sign-up")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()     // for dev
+//                        .requestMatchers(new AntPathRequestMatcher("/login")).permitAll()
+//                        .requestMatchers(new AntPathRequestMatcher("/api/v1/members/sign-up")).permitAll()
 //                        .requestMatchers(new AntPathRequestMatcher("/api/v1/notice", "POST")).hasRole(Role.ADMIN.getType())
 //                        .requestMatchers(new AntPathRequestMatcher("/api/v1/**")).hasRole(Role.CUSTOMER.getType())
                         .anyRequest().authenticated()
