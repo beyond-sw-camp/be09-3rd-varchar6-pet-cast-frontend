421a61f2535b759929e9b7c163d626ed0e5a24f9
jinjin0528 <jin12lee@naver.com>
2024-09-11 18:03:25 +0900

    fix: Request Category

--- backend/service-others/src/main/java/com/varchar6/petcast/serviceothers/domain/request/query/dto/RequestCategoryResponseDTO.java
+++ backend/service-others/src/main/java/com/varchar6/petcast/serviceothers/domain/request/query/dto/RequestCategoryResponseDTO.java
@@ -1,2 +1,15 @@
-package com.varchar6.petcast.serviceothers.domain.request.query.dto;public class RequestCategoryResponseDTO {
+package com.varchar6.petcast.serviceothers.domain.request.query.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class RequestCategoryResponseDTO {
+    private int categoryId;
+    private int requestId;
 }
