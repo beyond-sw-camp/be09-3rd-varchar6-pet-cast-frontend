7d5c5bd72085e6765322af79cf3aeed80f96ab37
woosuk1 <giuseog@gmail.com>
2024-09-13 15:07:27 +0900

    fix: pagination test 중

--- backend/service-others/src/main/java/com/varchar6/petcast/serviceothers/domain/notice/command/application/service/NoticeService.java
+++ backend/service-others/src/main/java/com/varchar6/petcast/serviceothers/domain/notice/command/application/service/NoticeService.java
@@ -2,9 +2,10 @@ package com.varchar6.petcast.serviceothers.domain.notice.command.application.ser
 
 import com.varchar6.petcast.serviceothers.domain.notice.command.application.dto.request.NoticeUpdateRequestDTO;
 import com.varchar6.petcast.serviceothers.domain.notice.command.application.dto.request.NoticeWriteRequestDTO;
+import org.springframework.stereotype.Service;
 
 
-
+@Service
 public interface NoticeService {
     int insertNotice(NoticeWriteRequestDTO noticeWriteRequestDTO) throws IllegalAccessException;
     int updateNotice(NoticeUpdateRequestDTO noticeUpdateRequestDTO) throws IllegalAccessException;
