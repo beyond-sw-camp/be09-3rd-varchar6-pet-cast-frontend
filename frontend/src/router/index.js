d90f0aa9872e7cbd645a7c14b824f36fd2dcd43a
jinjin0528 <jin12lee@naver.com>
2024-09-25 18:58:17 +0900

    merge stash

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/contents/Home.vue';
 import About from '../views/contents/About.vue';
-<<<<<<< Updated upstream
-import BizDetail from '../views/contents/biz_detail/BizDetail.vue';
-import BusinessEvents from '../views/contents/biz_detail/BusinessEvents.vue';
-import BusinessReviews from '../views/contents/biz_detail/BusinessReviews.vue';
-import BusinessQA from '../views/contents/biz_detail/BusinessQA.vue';
-=======
 import RequestWrite from '@/components/request/RequestWrite.vue';
->>>>>>> Stashed changes
 
 const routes = [
   {
@@ -22,30 +15,9 @@ const routes = [
     component: About,
   },
   {
-<<<<<<< Updated upstream
-    path: '/api/v1/companies/:id',
-    name: 'BizDetail',
-    component: BizDetail
-  },
-  {
-    path: '/business/:id/events',
-    name: 'BusinessEvents',
-    component: BusinessEvents
-  },
-  {
-    path: '/business/:id/reviews',
-    name: 'BusinessReviews',
-    component: BusinessReviews
-  },
-  {
-    path: '/business/:id/qa',
-    name: 'BusinessQA',
-    component: BusinessQA
-=======
     path: '/request',
     name: 'request',
     component: RequestWrite,
->>>>>>> Stashed changes
   },
 ];
 
