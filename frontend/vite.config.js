64d0a427cd9e5c4eb33ef5abe0dbe9ce3d52c9ed
gilhan <rlfgks97@gmail.com>
2024-09-26 10:18:43 +0900

    feat: add proxy settings for vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,11 +6,21 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+  //   server: {
+  //   proxy: {
+  //     // 예시: '/api'로 시작하는 요청을 프록시
+  //     '/api': {
+  //       target: 'http://localhost:8081', // 실제 API 서버 주소
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/oauth2/, '/oauth2') // "/api" 경로를 제거하고 프록시 요청
+  //     }
+  //   }
+  // }
 })
